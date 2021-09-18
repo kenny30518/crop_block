04931cdc63e48cb0c514ea6556ab250a50a5191f
D0746265 <paul776618@gmail.com>
2021-09-19 01:40:00 +0800

    2021/9/19
    paul776618 Update

--- start_server.bat
+++ start_server.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\Java\jre1.8.0_281\bin\java.exe" -Xmx1024M -Xms1024M -jar forge-1.12.2-14.23.5.2808-universal.jar nogui
+"C:\Program Files\Java\jre1.8.0_301\bin\java.exe" -Xmx1024M -Xms1024M -jar forge-1.12.2-14.23.5.2808-universal.jar nogui
 pause
